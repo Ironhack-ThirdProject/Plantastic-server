9a342c4d62766f25aeda589df4122a6c90935e45
Laura Bährs <laura.baehrs@googlemail.com>
2023-01-31 14:42:53 +0100

    add review model

---
+++ models/Review.model.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const reviewSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    productId: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Review = model("Review", reviewSchema);
+
+module.exports = Review;
---
+++ routes/review.routes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+const router = express.Router();
+const Product = require("../models/Product.model");
+const User = require("../models/Review.model");
+
+// GET all reviews of user
+router.get("/:userId", (req, res, next) => {
+
+});
+
+
+// POST: CREATE a review
+router.post("/", isAuthenticated, isCreator (req, res, next) => {
+
+});
+
+// GET a specific review
+router.get("/:reviewId", (req, res, next) => {
+
+});
+
+
+// EDIT a specific review
+router.put("/:reviewId", isAuthenticated, isCreator (req, res, next) => {
+
+});
+
+// DELETE a specific review
+router.delete("/:reviewId", isAuthenticated, isCreator, (req, res, next) => {
+  
+});
+
+
+
+module.exports = router;
